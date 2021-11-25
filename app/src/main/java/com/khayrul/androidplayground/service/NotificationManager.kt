f27aaac5fe07063db435e2b6fb294e128602190b
Khayrul <orion.beta.ori@gmail.com>
2021-11-25 13:15:39 +0600

    Notification listerer is added

--- app/src/main/java/com/khayrul/androidplayground/service/NotificationManager.kt
+++ app/src/main/java/com/khayrul/androidplayground/service/NotificationManager.kt
@@ -1,14 +1,19 @@
 package com.khayrul.androidplayground.service
 
-import android.app.NotificationChannel
+import android.app.*
+import androidx.core.app.RemoteInput
 import android.app.NotificationManager
 import android.content.Context
+import android.content.Intent
 import android.os.Build
+import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.khayrul.androidplayground.R
+import com.khayrul.androidplayground.presentation.MainActivity
 
 class NotificationManager {
+
     companion object {
         val CHANNEL_ID = "default_channel_id"
         val CHENNEL_NAME = "default_channel_name"
@@ -27,11 +32,28 @@ class NotificationManager {
 
         fun createNotification(context: Context, title: String, text: String) {
             createNotificationChannel(context)
+
+            val actionIntent = Intent(context, MainActivity::class.java)
+            actionIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            val pendingIntent = PendingIntent.getActivity(context, 0, actionIntent, PendingIntent.FLAG_ONE_SHOT)
+
+            val remoteInput = RemoteInput.Builder("quick_reply")
+                .setLabel("Reply")
+                .build()
+
+            val action = NotificationCompat.Action.Builder(
+                R.drawable.ic_reply,
+                "Reply",
+                pendingIntent
+            ).addRemoteInput(remoteInput).build()
+
             val notification = NotificationCompat.Builder(context, CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_fav)
                 .setContentTitle(title)
                 .setContentText(text)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .addAction(action)
+                .setAutoCancel(true)
                 .build()
 
             val notificationManager = NotificationManagerCompat.from(context)
