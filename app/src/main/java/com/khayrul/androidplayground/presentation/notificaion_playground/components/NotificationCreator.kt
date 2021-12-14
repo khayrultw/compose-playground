c2f20d5096c3fc91b4f9600bdd73d1909c02c7ca
Khayrul <orion.beta.ori@gmail.com>
2021-12-15 00:43:22 +0600

    popup is added

---
+++ app/src/main/java/com/khayrul/androidplayground/presentation/notificaion_playground/components/NotificationCreator.kt
@@ -0,0 +1,33 @@
+package com.khayrul.androidplayground.presentation.notificaion_playground.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Popup
+import androidx.compose.ui.window.PopupProperties
+
+@Composable
+fun NotificationCreator(
+    create: (title: String, text: String) -> Unit
+) {
+    Popup(
+        alignment = Alignment.Center,
+        properties = PopupProperties(
+            dismissOnBackPress = true,
+            dismissOnClickOutside = true
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .width(300.dp)
+                .background(Color.White)
+        ) {
+            Text(text = "Hello")
+        }
+    }
+}
